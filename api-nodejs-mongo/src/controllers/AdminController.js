e268ed8420cbf004dbc50b7a3143954670569551
Peter <124210296+Peter-Astralis@users.noreply.github.com>
2023-09-27 23:10:52 -0300

    Delete api-nodejs-mongo directory

--- api-nodejs-mongo/src/controllers/AdminController.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/users", (req, res) => {
-      console.log('controller')
-      return res.json({});
-})
-
-module.exports = router;
